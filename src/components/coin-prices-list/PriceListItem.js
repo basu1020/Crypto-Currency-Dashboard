7821d4a1746cd22e807079805d46330e573136dc
basu1020 <bvp1020mu@gmail.com>
2023-01-07 23:34:39 +0530

    updated some code

--- src/components/coin-prices-list/PriceListItem.js
+++ src/components/coin-prices-list/PriceListItem.js
@@ -3,12 +3,12 @@ import React, { useEffect } from 'react'
 const PriceListItem = (props) => {
   return (
     <>
-    <div className='flex flex-row justify-between mx-1  shadow-lg hover:shadow-xl bg-white rounded-lg my-1 '>
+    <div className='flex flex-row justify-between mx-1  shadow-lg hover:shadow-xl bg-white rounded-lg my-1'>
         <div className='mx-3 my-3'>
             <p className='font-bold text-gray-800'>{props.coinName}</p>
-            <p className='font-bold text-gray-500'>Mkt.Cap: ${props.coinMarketCap}</p>
+            <p className='font-bold text-gray-400'>Mkt.Cap: {Intl.NumberFormat('en-US', {style: 'currency', currency:'USD'}).format(props.coinMarketCap)}</p>
         </div>
-        <div className={
+        <div className = {
           props.percentageChange > 0 ? 'mx-4 flex flex-col justify-center font-bold text-green-600':'mx-4 flex flex-col justify-center font-bold text-red-500'}>
             {props.percentageChange} % 
         </div>
