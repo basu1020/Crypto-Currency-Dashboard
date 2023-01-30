2ee614ea76ff686ceedc95a3a3d9ab36d463115c
basu1020 <bvp1020mu@gmail.com>
2023-01-30 23:25:33 +0530

    wrote test cases for SearchBar component

--- src/components/price-chart/SearchBar.js
+++ src/components/price-chart/SearchBar.js
@@ -10,14 +10,14 @@ const SearchBar = () => {
 
     const onClickSearch = async () => {
         if (inpValue){
+            setShowSearchModal(true)
             const response = await fetch(`https://api.coingecko.com/api/v3/search?query=${inpValue}`)
             const data = await response.json()
             setSearchedCoins(data.coins.slice(0, 10))
-            setShowSearchModal(true)
             document.body.style.overflow = 'hidden'
         }
     }
-
+ 
     useEffect(() => {
 
     }, [showSearchedModal])
@@ -35,13 +35,13 @@ const SearchBar = () => {
                 </button>
             </div>
 
-            {showSearchedModal && <div className='absolute left-0 bottom-0 h-[100vh] w-[100%] bg-[rgb(0,0,0,0.5)] flex flex-col justify-center' 
+            {showSearchedModal ? <div className='absolute left-0 bottom-0 h-[100vh] w-[100%] bg-[rgb(0,0,0,0.5)] flex flex-col justify-center' name='searchModal'
             onClick={() => {
                 setShowSearchModal(false)
                 document.body.style.overflow = 'auto'
             }}>
                 <div className="h-[60vh] overflow-x-auto border border-blue-400 w-[70%] mx-auto rounded-md bg-white p-1">
-                    <h2 className='mx-2 my-2 text-lg font-bold'>Search Results</h2>
+                    <p className='mx-2 my-2 text-lg font-bold'data-testid="search-results-heading">Search Results</p>
                     <div className='flex flex-col justify-center flex-wrap'>
                         {searchedCoins.map((element, index) => {
                             return (<div className='h-auto p-1 hover:bg-blue-500 hover:text-white'
@@ -59,7 +59,7 @@ const SearchBar = () => {
                         })}
                     </div>
                 </div>
-            </div>}
+            </div> : null}
         </>
     )
 }
--- src/tests/BaseCurrencyComponent.test.js
+++ src/tests/BaseCurrencyComponent.test.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
-import { userEvent } from '@testing-library/user-event'
 import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import BaseCurrencyOptions from '../components/Price-chart/BaseCurrencyOptions'
